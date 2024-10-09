d076f0407a18199ccfe7f02c04ec7444f29ab89c
atomicwrite <atomicbreathgodzilla@gmail.com>
2024-10-09 13:42:52 -0500

    Clodo KEys

--- CloodKey/Program.cs
+++ CloodKey/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using CommandLine;
 using System.Collections.Generic;
+using CloodKey.Interfaces;
+using CloodKey.Implementations;
+using CloodKey.Options;
 
 namespace CloodKey
 {
@@ -8,9 +11,47 @@ namespace CloodKey
     {
         static void Main(string[] args)
         {
-           
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
         }
 
-       
+        static void RunOptions(CommandLineOptions opts)
+        {
+            try
+            {
+                IKeyCLI keyCLI = opts.CredentialStore.ToLower() switch
+                {
+                    "aws" => new AwsKeyCLI(),
+                    "azure" => new AzureKeyCLI(),
+                    "os" => new OsKeyCLI(),
+                    _ => throw new ArgumentException("Invalid credential store specified.")
+                };
+
+                if (!keyCLI.ValidateCliTool())
+                {
+                    Console.WriteLine($"The CLI tool for {opts.CredentialStore} is not installed or not found in the system PATH.");
+                    return;
+                }
+
+                string result = opts.Operation.ToLower() switch
+                {
+                    "get" => keyCLI.Get(opts.Key),
+                    "set" => keyCLI.Set(opts.Key, opts.Value),
+                    _ => throw new ArgumentException("Invalid operation specified.")
+                };
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            Console.WriteLine("Invalid command line arguments. Please check the usage and try again.");
+        }
     }
 }
