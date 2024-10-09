483108e5e15cfa11ca939935d2deab7fa93d4a0b
atomicwrite <atomicbreathgodzilla@gmail.com>
2024-10-08 20:04:07 -0500

    clood

--- Clood/Doodle.cs
+++ Clood/Doodle.cs
@@ -12,6 +12,11 @@ public class Doodle
         return a - b;
     }
 
+    public double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
     public int Multiply(int a, int b)
     {
         return a * b;
