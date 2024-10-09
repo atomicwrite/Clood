ef71417c0e5c42d1600e0da2b2ee7471c524ce1b
atomicwrite <atomicbreathgodzilla@gmail.com>
2024-10-09 15:36:44 -0500

    Changes made by Claudia AI on branch Modifications-CmdKeyClicsKeyClics-1

---
+++ CloodTest/CmdKeyCliTests.cs
@@ -0,0 +1,58 @@
+using System;
+using NUnit.Framework;
+using CloodKey;
+using CloodKey.Interfaces;
+
+namespace CloodTest
+{
+    [TestFixture]
+    public class CmdKeyCliTests
+    {
+        private CmdKeyCli _cmdKeyCli;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cmdKeyCli = new CmdKeyCli();
+        }
+
+        [Test]
+        public void TestAddingKey()
+        {
+            // Arrange
+            string key = "TestKey";
+            string value = "TestValue";
+
+            // Act
+            string result = _cmdKeyCli.Set(key, value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Key set successfully."));
+        }
+
+        [Test]
+        public void TestAddingAndGettingKey()
+        {
+            // Arrange
+            string key = "TestKey2";
+            string value = "TestValue2";
+
+            // Act
+            _cmdKeyCli.Set(key, value);
+            string retrievedValue = _cmdKeyCli.Get(key);
+
+            // Assert
+            Assert.That(retrievedValue, Is.EqualTo(value));
+        }
+
+        [Test]
+        public void TestGettingNonExistentKey()
+        {
+            // Arrange
+            string nonExistentKey = "NonExistentKey";
+
+            // Act & Assert
+            Assert.Throws<KeyNotFoundException>(() => _cmdKeyCli.Get(nonExistentKey));
+        }
+    }
+}
