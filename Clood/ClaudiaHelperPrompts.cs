f0156d555e68fd85eb0524de6eeb35b59a38b557
atomicwrite <atomicbreathgodzilla@gmail.com>
2024-10-09 11:43:20 -0500

    Clood

--- Clood/ClaudiaHelperPrompts.cs
+++ Clood/ClaudiaHelperPrompts.cs
@@ -4,14 +4,14 @@ public static class ClaudiaHelperPrompts
 {
     public static string FormatCodeHelperPrompt(string filesDict, string prompt, string rootFolder)
         => $$"""
-             You are tasked with applying a specific prompt to multiple code files and returning the modified contents in a JSON format. Here's how to proceed:
+             You are tasked with applying a specific prompt within the context multiple code files and returning the modified contents in a JSON format. Here's how to proceed:
 
              1. You will be given a dictionary where the filename is the key and the value is the content, a prompt to apply, and the contents of multiple code files.
 
              2. The file dictionary is as follows:
              <file_dictionary>
              {{filesDict}}
-              </file_dictionary>
+              </file_dictionary> 
 
              3. The prompt to apply to each file is:
              <prompt>
@@ -21,13 +21,14 @@ public static class ClaudiaHelperPrompts
              4. Read all the files, some may not need to be changed and are just there for context:
                a. Generate the modified content based on the prompt
                b. If new files need to be created, include them in the output
+               c. The Root folder is <root_folder>{{rootFolder}} </root_folder>.
+               d. If you are asked to put new file in a specific folder, try to figure it out based on the root_folder.
+               e. All files should have the same root folder as root_folder.
 
              5. After processing all files, format your response as a JSON object with two arrays:
                 - "changedFiles": An array of objects, each containing "filename" and "content" for modified existing files
                 - "newFiles": An array of objects, each containing "filename" and "content" for newly created files
-                - The Root folder is <root_folder>{{rootFolder}} </root_folder>.
-                - If you are asked to put new file in a specific folder, try to figure it out based on the root_folder.
-                - All files should have the same root folder as root_folder. 
+                - Do not include a file if it has not been changed. 
                 
              6. Ensure that the JSON is properly formatted and escaped, especially for multi-line code contents.
 
