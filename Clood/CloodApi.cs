0d01f7489536703a91da285558030bfd8232d6c7
atomicwrite <atomicbreathgodzilla@gmail.com>
2024-10-10 16:42:14 +0000

    Clood

--- Clood/CloodApi.cs
+++ Clood/CloodApi.cs
@@ -9,12 +9,86 @@ namespace Clood;
 public static class CloodApi
 {
     public static string GitRoot { get; set; } = null!;
+
     public static void ConfigureApi(WebApplication app, string gitRoot)
     {
-        app.UseExceptionHandler()(ap)
-        app.MapPost("/api/clood/start", CreateCloodApi.CreateCloodChanges);
-        app.MapPost("/api/clood/merge", MergeCloodApi.MergeCloodChanges);
-        app.MapPost("/api/clood/revert", RevertCloodApi.RevertCloodChanges);
-        GitRoot = gitRoot; 
+        app.MapPost("/api/clood/start", async ([FromBody] CloodRequest request) =>
+        {
+            try
+            {
+                return await CreateCloodApi.CreateCloodChanges(request);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return CloodStartErrorResponse(e);
+            }
+        });
+        app.MapPost("/api/clood/merge", async ([FromBody] MergeRequest request) =>
+        {
+            try
+            {
+                return await MergeCloodApi.MergeCloodChanges(request);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return CloodMergeErrorResponse(e);
+            }
+        });
+        app.MapPost("/api/clood/revert", async ([FromBody] string request) =>
+        {
+            try
+            {
+                return await RevertCloodApi.RevertCloodChanges(request);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return CloodMergeRevertResponse(e);
+            }
+        });
+ 
+        GitRoot = gitRoot;
+    }
+
+    private static IResult CloodMergeRevertResponse(Exception e)
+    {
+        return Results.Ok(new CloodResponse<string>()
+        {
+            Data = "",
+            ErrorMessage = e.Message,
+            Success = false
+        });
+    }
+
+    private static IResult CloodMergeErrorResponse(Exception e)
+    {
+        return Results.Ok(new CloodResponse<string>()
+        {
+            Data = "",
+            ErrorMessage = e.Message,
+            Success = false
+        });
+    }
+
+    private static IResult CloodStartErrorResponse(Exception e)
+    {
+        return Results.Ok(new CloodResponse<CloodStartResponse>()
+        {
+            Data = new CloodStartResponse()
+            {
+                Id = "-1",
+                NewBranch = "",
+                ProposedChanges = new FileChanges()
+                {
+                    Answered = false,
+                    ChangedFiles = [],
+                    NewFiles = [],
+                }
+            },
+            ErrorMessage = e.Message,
+            Success = false
+        });
     }
 }
