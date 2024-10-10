474da702728125e718f958838b605ed5aff97dc2
atomicwrite <atomicbreathgodzilla@gmail.com>
2024-10-10 06:30:51 -0500

    Added Logger

--- Clood/Program.cs
+++ Clood/Program.cs
@@ -1,17 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using CliWrap;
-using CliWrap.Buffered;
-using Microsoft.Extensions.Configuration;
-using Claudia;
-using Markdig;
-using Markdig.Syntax;
 using CommandLine;
 using Serilog;
 
