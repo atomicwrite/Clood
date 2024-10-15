82a1be927f432f65658f3531b200cdf0ea784c3e
atomicwrite <atomicbreathgodzilla@gmail.com>
2024-10-15 20:09:11 +0000

    Github temp folders

--- CloodTest/CloodFileMapTestsHelper.cs
+++ CloodTest/CloodFileMapTestsHelper.cs
@@ -5,7 +5,10 @@ public static class CloodFileMapTestsHelper
     public static string GetTempPath()
     {
         var environmentVariable = Environment.GetEnvironmentVariable("RUNNER_TEMP");
-
+        if (environmentVariable != null)
+        {
+            environmentVariable += environmentVariable + "/tmp";
+        }
         return environmentVariable ?? Path.GetTempPath();
     }
 }
